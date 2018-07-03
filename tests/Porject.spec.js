576a5312cac9392fb148f8de350e77b94daf9893
zslxbl <zslxbl@163.com>
2018-07-03 22:22:55 +0800

    updatef

--- tests/Porject.spec.js
+++ tests/Porject.spec.js
@@ -22,14 +22,14 @@ describe('Project Contract', () => {
 
         //1.2 部署PojectList 合约
         projectList = await new web3.eth.Contract(JSON.parse(ProjectList.interface))
-                    .deploy({data : ProjectList.bytecode})
-                    .send({from: accounts[0], gas: '5000000'});
+            .deploy({ data: ProjectList.bytecode })
+            .send({ from: accounts[0], gas: '5000000' });
         //1.3 调用 ProjectList 的  createProject 方法
         await projectList.methods.createProject('Ethereum DApp Tutorial', 100, 10000, 1000000)
-        .send({
-            from: accounts[0],
-            gas: '1000000',
-        });
+            .send({
+                from: accounts[0],
+                gas: '1000000',
+            });
 
         //1.4 获取刚创建的project 实例地址
         const [address] = await projectList.methods.getProjects().call();
@@ -39,12 +39,12 @@ describe('Project Contract', () => {
 
     });
 
-    it('should deploy ProjectList and Project', async ()=> {
+    it('should deploy ProjectList and Project', async () => {
         assert.ok(projectList.options.addrsss);
         assert.ok(project.options.address);
     });
 
-    it('should save correct project properties', async ()=> {
+    it('should save correct project properties', async () => {
         const owner = await project.methods.owner().call();
         const description = await project.methods.description().call();
         const minInvest = await project.methods.minInvest().call();
@@ -59,6 +59,42 @@ describe('Project Contract', () => {
 
     });
 
+    it('should allow investor to contribute', async () => {
+        const investor = accounts[1];
+        await project.methods.contributed().send({
+            from: investor,
+            value: '200',
+        });
+        const amount = await project.methods.investors(investor).call();
+        assert.ok(amount == '200')''
+    });
+
+    it('should require minInvest', async() => {
+        try {
+            const investor = accounts[1];
+            await project.methods.contribute().send({
+                from: investor,
+                value: '10',
+            });
+            assert.ok(false);
+        } catch (err) {
+            assert.ok(err);
+        }
+    });
+
+    it('should require maxInvest', async() => {
+        try {
+            const investor = accounts[1];
+            await project.methods.contribute().send({
+                from: investro,
+                value: '100000',
+            });
+            assert.ok(false);
+
+        } catch (err) {
+            assert.ok(err);            
+        }
+    });
 
 
 
